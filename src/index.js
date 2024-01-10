1c1b2e87a0cd444e851e52041cfc7322d6e5e5e5
sonupk <sonukamble1121@example.com>
2024-01-10 21:00:52 +0530

    Done with index.js file

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const route = require('./routes/auth.js');
+
+const { default: mongoose } = require('mongoose');
+
+const app = express();
+
+
+app.use(express.json());
+
+
+
+mongoose.connect("mongodb+srv://sonuk:kamble123@cluster0.vfrmzq9.mongodb.net/snabbtech-DB" )
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
+
+    
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
